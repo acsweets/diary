715667b0880f50ddd12338266f451a0691617373
sweetac <1395723441@qq.com>
2023-06-29 01:13:04 +0800

    家里环境给我人配麻了

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner :false,
-      title: '心情日记',
+      title: '心情日记  ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
