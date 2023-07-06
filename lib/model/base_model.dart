aa5d9e3541a6f198223fe56cb10df22f0141c1ff
liuxiaocao <liuxiaocao@wehanyu.com>
2023-07-06 17:25:31 +0800

    fix

--- lib/model/base_model.dart
+++ lib/model/base_model.dart
@@ -1,15 +1,14 @@
-
+///感觉最近都没什么脑子，无奈
 import 'package:flutter/material.dart';
 
 class BaseModel extends ChangeNotifier{
 
   BuildContext? context;
 
-
+  ///设置下上下文
   void setContext(BuildContext context) {
     if (this.context == null) {
       this.context = context;
     }
   }
-
 }
