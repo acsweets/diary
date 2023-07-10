b6aac30f6871f6af556b4d2d305793fbaad21621
liuxiaocao <liuxiaocao@wehanyu.com>
2023-07-10 16:36:13 +0800

    国际化

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,6 +10,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text( AppLocalizations.of(context)!.colorDialogTitle),
+    );
   }
 }
